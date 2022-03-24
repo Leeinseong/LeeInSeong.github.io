519cb0a86a6f00f0ceff100fcf159379c521265e
LeeInSeong <dlstjd0117@ajou.ac.kr>
2022-03-25 00:56:33 +0900

    add Head from next/head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Inseong_Portofolio</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
